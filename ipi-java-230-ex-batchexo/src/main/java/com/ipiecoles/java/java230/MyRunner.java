484963961cb11d643737fa148496e99cfcb1ade6
julienudev <julien.udev@gmail.com>
2020-01-26 17:22:43 +0100

    test ok

--- ipi-java-230-ex-batchexo/src/main/java/com/ipiecoles/java/java230/MyRunner.java
+++ ipi-java-230-ex-batchexo/src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
@@ -35,7 +36,7 @@ public class MyRunner implements CommandLineRunner {
     private static final int NB_CHAMPS_TECHNICIEN = 7;
     private static final String REGEX_MATRICULE_MANAGER = "^M[0-9]{5}$";
     private static final int NB_CHAMPS_COMMERCIAL = 7;
-    private static final String REGEX_TYPE="^[MTC]{1}.*";
+    private static final String REGEX_IsMTC = "^[MTC]{1}.*";
 
     @Autowired
     private EmployeRepository employeRepository;
@@ -49,13 +50,17 @@ public class MyRunner implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        String fileName = "employes.csv";
-        readFile(fileName);
-        //readFile(strings[0]);
+        try {
+            String fileName = "employes.csv";
+            readFile(fileName);
+        } catch (FileNotFoundException fileNotFound) {
+            System.out.println(fileNotFound.getMessage() + " => fichier introuvable à cet emplacement");
+        }
     }
 
     /**
      * Méthode qui lit le fichier CSV en paramètre afin d'intégrer son contenu en BDD
+     *
      * @param fileName Le nom du fichier (à mettre dans src/main/resources)
      * @return une liste contenant les employés à insérer en BDD ou null si le fichier n'a pas pu être le
      */
@@ -63,24 +68,13 @@ public class MyRunner implements CommandLineRunner {
         Stream<String> stream;
         stream = Files.lines(Paths.get(new ClassPathResource(fileName).getURI()));
         //TODO
-        Integer i =0;
-        for (String ligne : stream.collect(Collectors.toList())){
+        Integer i = 0;
+        for (String ligne : stream.collect(Collectors.toList())) {
             i++;
             try {
                 processLine(ligne);
-                if (ligne.charAt(0)=='M'){
-                    processManager(ligne);
-                }
-                else if (ligne.charAt(0)=='C'){
-                    processCommercial(ligne);
-                }
-                else if (ligne.charAt(0)=='T'){
-                    processTechnicien(ligne);
-                }
-
-            }
-            catch (BatchException e ){
-                System.out.println("ligne"+i+" : "+e.getMessage()+" => "+ligne);
+            } catch (BatchException batchExc) {
+                System.out.println("ligne" + i + " : " + batchExc.getMessage() + " => " + ligne);
             }
         }
         return employes;
@@ -89,195 +83,169 @@ public class MyRunner implements CommandLineRunner {
 
     /**
      * Méthode qui regarde le premier caractère de la ligne et appelle la bonne méthode de création d'employé
+     *
      * @param ligne la ligne à analyser
      * @throws BatchException si le type d'employé n'a pas été reconnu
      */
-    private void processLine(String ligne) throws BatchException {
-        //TODO
-        //int n=0;
+    private void processLine(String ligne) throws Exception {
 
 
-//EXPRESSION REGULIERE / VERIFICATION TYPE D'EMPLOYE
+//VERIFICATION TYPE D'EMPLOYE
 
-        ligne.matches(REGEX_TYPE);
-        if (!ligne.matches(REGEX_TYPE)) {
+        ligne.matches(REGEX_IsMTC);
+        if (!ligne.matches(REGEX_IsMTC)) {
             throw new BatchException(("type d'employe inconnu" + ligne.charAt(0)));
         }
-
         String array[] = ligne.split(",");
-        if (array.length > 0) {
-//EXPRESSION REGULIERE / VERIFICATION MATRICULE
 
-        //if(arr[0].length()!=6){
-        if (!array[0].matches(REGEX_MATRICULE)){
-            throw new BatchException("la chaîne "+ array[0] +" ne respecte pas l'expression régulière ^[MTC][0-9]{5}$");
+//EXPRESSION REGULIERE / VERIFICATION FORMAT DU MATRICULE
+
+        if (!array[0].matches(REGEX_MATRICULE)) {
+            throw new BatchException("la chaîne " + array[0] + " ne respecte pas l'expression régulière ^[MTC][0-9]{5}$");
+        }
+
+// APPEL DE LA BONNE METHODE EN FONCTION DU TYPE D'EMPLOYE
+        switch (ligne.charAt(0)) {
+            case 'M':
+                processManager(ligne);
+                break;
+            case 'C':
+                processCommercial(ligne);
+                break;
+            case 'T':
+                processTechnicien(ligne);
+                break;
         }
 
 
-            //TEST FORMAT DATE
-
-            try {
-                String date = array[3];
-                //System.out.println(date);
-                DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(date);
-            }
-            catch (ArrayIndexOutOfBoundsException e) {
-                throw new BatchException("pas de date");
-            }
-            catch (Exception e) {
-                throw new BatchException(ligne + " ne respecte pas le format de date dd/MM/yyyy ");
-            }
 //TESTE DU SALAIRE
 
-           /* if (array.length >= 4) {
-                //System.out.println(arr.length);
-                char salaire = array[4].charAt(0);
-                if (!Character.isDigit(salaire)) {
-                    throw new BatchException("sdf n'est pas un nombre valide pour un salaire");
-                }
-            }*/
-
-           try {
-               char salaire = array[4].charAt(0);
-               Character.isDigit(salaire);
-           }
-           catch (ArrayIndexOutOfBoundsException e){
-               throw new ArrayIndexOutOfBoundsException("pas de salaire");
-
-           }
-           catch(Exception e){
-               throw new BatchException("sdf n'est pas un nombre valide pour un salaire");
-           }
+        try {
+            if (!(array[4] == null || array[4].length() == 0)) {
+                Float.parseFloat(array[4]);
+            }
 
 
+            //System.out.println(Integer.parseInt(array[4]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ArrayIndexOutOfBoundsException(array[4] + " =>pas de salaire");
 
+        } catch (Exception e) {
+            throw new BatchException(array[4] + " => n'est pas un nombre valide pour un salaire");
+        }
+
+
+        //TEST FORMAT DATE
+
+        try {
+            String date = array[3];
+            DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(date);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new BatchException("pas de date");
+        } catch (Exception e) {
+            throw new BatchException(ligne + " ne respecte pas le format de date dd/MM/yyyy ");
         }
     }
 
     /**
      * Méthode qui crée un Commercial à partir d'une ligne contenant les informations d'un commercial et l'ajoute dans la liste globale des employés
+     *
      * @param ligneCommercial la ligne contenant les infos du commercial à intégrer
      * @throws BatchException s'il y a un problème sur cette ligne
      */
     private void processCommercial(String ligneCommercial) throws BatchException {
-        //TODO
-        int numberOfWord = ligneCommercial.split(",").length;
-        //if(arr[0].length()!=6){
-        if (numberOfWord != 7){
-            throw new BatchException("La ligne manager ne contient pas 5 éléments mais "+numberOfWord);
-        }
-        if (numberOfWord != 7){
-            throw new BatchException("La ligne manager ne contient pas 5 éléments mais "+numberOfWord);
+
+        // VERIFICATION DU NB DE CHAMP DE COMMERCIAL
+
+        String arrayCommercial[] = ligneCommercial.split(",");
+        if (arrayCommercial.length != NB_CHAMPS_COMMERCIAL) {
+            throw new BatchException("La ligne commercial ne contient pas 5 éléments mais " + arrayCommercial.length);
         }
 
-        //TESTE DU CHIFFRE DAFFAIRE
-        String arr[] = ligneCommercial.split((","));
+        // TESTE DU CHIFFRE DAFFAIRE
 
-
-
-
-        if (numberOfWord >= 6) {
-            //System.out.println(arr.length);
-           char chiffreDaffaire = arr[5].charAt(0);
-            if (!Character.isDigit(chiffreDaffaire)) {
-                throw new BatchException("Le chiffre d'affaire du commercial est incorrect : " + chiffreDaffaire);
-            }
-
-//TESTE DE LA PERFORMANCE
-            if (numberOfWord >= 7) {
-                char performance = arr[6].charAt(0);
-                if (!Character.isDigit(performance)) {
-                    throw new BatchException("La performance du commercial est incorrect : " + performance);
-                }
-            }
+        char chiffreDaffaire = arrayCommercial[5].charAt(0);
+        if (!Character.isDigit(chiffreDaffaire)) {
+            throw new BatchException("Le chiffre d'affaire du commercial est incorrect : " + chiffreDaffaire);
         }
 
+        // TESTE DE LA PERFORMANCE
 
-
+        char performance = arrayCommercial[6].charAt(0);
+        if (!Character.isDigit(performance)) {
+            throw new BatchException("La performance du commercial est incorrect : " + performance);
+        }
     }
+
 
     /**
      * Méthode qui crée un Manager à partir d'une ligne contenant les informations d'un manager et l'ajoute dans la liste globale des employés
+     *
      * @param ligneManager la ligne contenant les infos du manager à intégrer
      * @throws BatchException s'il y a un problème sur cette ligne
      */
     private void processManager(String ligneManager) throws BatchException {
-//TODO
-//NOMBRE ELEMENT DE LA LIGNE
 
-        int numberOfWord = ligneManager.split(",").length;
-        //if(arr[0].length()!=6){
-        if (numberOfWord != 5){
-            throw new BatchException("La ligne manager ne contient pas 5 éléments mais "+numberOfWord);
+        // VERIFICATION NB DE CHAMP MANAGER
+
+        String arrayManager[] = ligneManager.split(",");
+        if (arrayManager.length != NB_CHAMPS_MANAGER) {
+            throw new BatchException("La ligne manager ne contient pas 5 éléments mais " + arrayManager.length);
         }
-        if (numberOfWord != 5){
-            throw new BatchException("La ligne manager ne contient pas 5 éléments mais "+numberOfWord);
-        }
-
-
     }
+
 
     /**
      * Méthode qui crée un Technicien à partir d'une ligne contenant les informations d'un technicien et l'ajoute dans la liste globale des employés
+     *
      * @param ligneTechnicien la ligne contenant les infos du technicien à intégrer
      * @throws BatchException s'il y a un problème sur cette ligne
      */
-    private void processTechnicien(String ligneTechnicien) throws BatchException {
-        //TODO
-        //int numberOfWord = ligneTechnicien.split(",").length;
-        String arr[] = ligneTechnicien.split(",");
-        //if(arr[0].length()!=6){
+    private void processTechnicien(String ligneTechnicien) throws Exception {
 
+        // VERIFICATION NB DE CHAMP TECHNICIEN
 
-        if (ligneTechnicien.split(",").length == 6) {
-            throw new BatchException("La ligne technicien ne contient pas 7 éléments mais " + 6);
+        String arrayTechnicien[] = ligneTechnicien.split(",");
+        if (arrayTechnicien.length != NB_CHAMPS_TECHNICIEN) {
+            throw new BatchException("La ligne technicien ne contient pas 5 éléments mais " + arrayTechnicien.length);
+        }
+
+        // VERIFICATION GRADE
+
+        Integer grade;
+        try {
+            grade = Integer.parseInt(arrayTechnicien[5]);
+        } catch (Exception e) {
+            throw new BatchException("Le grade du technicien est incorrect : " + arrayTechnicien[5]);
+        }
+        if (grade < 1 || grade > 5) {
+            throw new BatchException("Le grade doit être compris entre 1 et 5 : " + grade);
         }
 
 
-        if (ligneTechnicien.split(",").length == 9) {
-            throw new BatchException("La ligne technicien ne contient pas 7 éléments mais " + 9);
+        // VERIFICATION DU FORMAT DU MANAGER ATTRIBUE AU TEXCHNICIEN
+
+        if (!arrayTechnicien[6].matches(REGEX_MATRICULE_MANAGER)) {
+            throw new BatchException("La chaîne " + arrayTechnicien[6] + " ne respecte pas l'expression régulière ^M[0-9]{5}$ ");
         }
 
+        //VERIFICATION EXISTENCE DU MANAGER EN DB
 
-        //TESTE GRADE
-
-        if (ligneTechnicien.split(",").length >= 7) {
-            char grade = arr[5].charAt(0);
-            //System.out.println(grade);
-
-            if (Character.isDigit(grade)) {
-                throw new BatchException(("Le grade du technicien est incorrect"));
+        if (arrayTechnicien[6].matches(REGEX_MATRICULE_MANAGER)) {
+            Employe employe = employeRepository.findByMatricule(arrayTechnicien[6]);
+            if (employe == null) {
+                throw new BatchException("Le manager de matricule " + arrayTechnicien[6] + " n'a pas été trouvé dans en base de données");
             }
-
-            if (grade < 0 || grade > 5) {
-                throw new BatchException(("Le grade doit être compris entre 1 et 5 : " +grade));
-            }
-
-
-            String matriManag = arr[6];
-            if (!matriManag.matches(REGEX_MATRICULE_MANAGER)) {
-                throw new BatchException("la chaîne xxx ne respecte pas l'expression régulière");
-
-            }
-            try {
-                if (employeRepository.findByMatricule(matriManag) != null){};
-                /*if (e.getMatricule() == arr[6]) {
-                    throw new BatchException("Le manager de matricule n'a pas été trouvé dans le fichier ou en base de données");
-                }*/
-
-            }
-            catch (NullPointerException n)
-                        {
-                            throw new NullPointerException("Le manager de matricule n'a pas été trouvé dans le fichier ou en base de données");
-                        }
         }
     }
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
